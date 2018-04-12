dc50bcafb905b02be97a6aa14b27fc8a5e19ec5a
Liam Muller <liam.w.muller@gmail.com>
2018-04-12 19:28:39 -0400

    Allow CORS, Capture too many requests error

--- server.js
+++ server.js
@@ -2,8 +2,25 @@ var express = require('express');
 var https = require('https');
 var bodyParser = require('body-parser');
 
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, Content-Length, X-Requested-With'
+  );
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+};
+
 var app = express();
 app.use(bodyParser.json());
+app.use(allowCrossDomain);
 
 var server = app.listen(process.env.PORT || 8080, function() {
   var port = server.address().port;
@@ -38,19 +55,23 @@ app.get('/api/player/:id', function(req, res) {
         headers
       },
       response => {
-        response.on('data', d => {
-          const rawPlayer = JSON.parse(d);
-          if (rawPlayer.data && rawPlayer.data.length > 0) {
-            const player = {
-              name: rawPlayer.data[0].attributes.name,
-              id: rawPlayer.data[0].id,
-              matches: rawPlayer.data[0].relationships.matches.data
-            };
-            res.status(200).json({ player });
-          } else {
-            res.status(404).json({ player: null, error: rawPlayer });
-          }
-        });
+        if (response.statusCode === 429) {
+          res.status(404).json({ player: null, error: 'Too many requests' });
+        } else {
+          response.on('data', d => {
+            const rawPlayer = JSON.parse(d);
+            if (rawPlayer.data && rawPlayer.data.length > 0) {
+              const player = {
+                name: rawPlayer.data[0].attributes.name,
+                id: rawPlayer.data[0].id,
+                matches: rawPlayer.data[0].relationships.matches.data
+              };
+              res.status(200).json({ player });
+            } else {
+              res.status(404).json({ player: null, error: rawPlayer });
+            }
+          });
+        }
       }
     )
     .on('error', e => {
